3b863886c27677e7a12aab1317aaae6047219df4
fengtuo58 <fengtuo58@qq.com>
2019-07-01 21:18:42 +0800

    lambda learning

--- TestForinterView/String.hpp
+++ TestForinterView/String.hpp
@@ -10,15 +10,65 @@
 #define String_hpp
 #include <string.h>
 #include <stdio.h>
-
-#endif /* String_hpp */
-class String
+#include <iostream>
+#include <string>
+using namespace std;
+class String1
 {
 public:
-    String(const char *str = NULL); // 普通构造函数
-    String(const String &other); // 拷贝构造函数
-    ~ String(void); // 析构函数
-    String & operator =(const String &other); // 赋值函数
+    String1(const char *str = NULL); // 普通构造函数
+    String1(const String1 &other); // 拷贝构造函数
+    ~ String1(void); // 析构函数
+    String1 & operator =(const String1 &other); // 赋值函数
+    friend ostream& operator << (ostream &cou, String1 &str);
 private:
     char *m_data; // 用于保存字符串
+    const int m;
+    const int &ref;
 };
+ostream& operator << (ostream &cou, String1 &str)
+{
+    cou << "www" << strlen(str.m_data) << "eee";
+    return cou;
+}
+String1::String1(const char *str):m(0),ref(m)
+{
+    if(str==NULL)
+    {
+        m_data = new char[1];   // 得分点：对空字符串自动申请存放结束标志'\0'的空
+        *m_data = '\0';         //加分点：对m_data加NULL 判断
+    }
+    else
+    {
+        int length = strlen(str);
+        m_data = new char[length+1];    // 若能加 NULL 判断则更好
+        strcpy(m_data, str);
+    }
+}
+// String1的析构函数
+String1::~String1(void)
+{
+    delete [] m_data;
+}
+//拷贝构造函数
+String1::String1(const String1 &other):m(0),ref(m) // 得分点：输入参数为const型
+{
+    int length = strlen(other.m_data);
+    m_data = new char[length+1];    //加分点：对m_data加NULL 判断
+    strcpy(m_data, other.m_data);
+}
+//赋值函数
+String1 & String1::operator =(const String1 &other) // 得分点：输入参数为const型
+{
+    if(this == &other)  //得分点：检查自赋值
+        return *this;
+    delete [] m_data;   //得分点：释放原有的内存资源
+    int length = strlen( other.m_data );
+    m_data = new char[length+1];    //加分点：对m_data加NULL 判断
+    strcpy( m_data, other.m_data );
+    return *this;   //得分点：返回本对象的引用
+}
+#endif /* String_hpp */
+
+
+
--- TestForinterView/main.cpp
+++ TestForinterView/main.cpp
@@ -2,7 +2,9 @@
 #include <iostream>
 using namespace std;
 #include <typeinfo>
-
+#include <cmath>
+//extern class String;
+#include "String.hpp"
 void f(char const *ptr)
 {
     cout << "1" << endl;
@@ -88,6 +90,13 @@ classA operator*(const classA& a1,const classA& a2)
     return classA();
 }
 
+void funA(int mm[10])
+{
+    cout << endl <<sizeof(mm) << endl;
+}
+struct MemTest{
+    
+};
 int main(int argc, char *argv[])
 {
 //    A s = func();
@@ -103,8 +112,63 @@ int main(int argc, char *argv[])
  //   bn.func2();
  
    // operator*的返回结果必须是一个const对象。如果不是，这样的变态代码也不会编译出错：//
-    classA a, b, c;
-    (a * b) = c; // 对a*b的结果赋值
-   // 操作(a * b) = c显然不符合编程者的初衷，也没有任何意义。
+//    classA a, b, c;
+//    (a * b) = c; // 对a*b的结果赋值
+//   // 操作(a * b) = c显然不符合编程者的初衷，也没有任何意义。
+   String1 s; //类或结构体的前向声明只能用来定义指针对象，因为编译到这里时还没有发现定义，不知道该类或者结构的内部成员，没有办法具体的构造一个对象，所以会报错。 将类成员改成指针就好了
+    cout << s;
+    int m[10];
+    //注意当数组作为函数的参数进行传递时，该数组自动退化为同类型的指针
+    funA(m);
+    float a, b;
+    a = 2.5000000000001;//编译器自己加e 极小值
+    b= 2.50;
+    float zero = a - b;
+    if (zero == 0) {
+        cout << "ok";
+    }
+    char aa[] = "hello";
+    aa[0] = 'X';
+    char *p = "world"; // 注意p 指向常量字符串
+//    p[0] = 'X';
+    MemTest *mTest1=new MemTest[10];
+    
+    MemTest *mTest2=new MemTest;
+    
+    int *pInt1=new int [10];
+    
+    int *pInt2=new int;
+    
+    delete[]pInt1; //-1-
+    
+    delete[]pInt2; //-2-
+    
+    delete[]mTest1;//-3-
+    
+    delete[]mTest2;//-4-
+//    std::cout << [](float f) { return std::abs(f); }(-3.5);
+//    std::cout << [](float f) -> int { return std::abs(f); } (-3.5);
+//    auto lambda = [](int f) ->int {return f * f;};
+//    cout << lambda(5);
+//    std::function<int()> lambda1 = [] () -> int { return 100 * 100; } ;
+//    float f01 = 1.0;
+//    std::cout << [=](float f) { return f01 + std::abs(f); } (-3.5) << endl;
+//    float f0 = 1.0;
+//    std::cout << [&](float f) mutable { return f0 += std::abs(f); } (-3.5);//fo is a copy for fo in outsides
+//    //C++ 11 标准说，如果以传值的形式捕获外部变量，那么，lambda 体不允许修改外部变量，对 f0 的任何修改都会引发编译错误。
+//    std::cout << '\n' << f0 << '\n';
+    
+    float f0 = 1.0f;
+    float f1 = 10.0f;
+    std::cout << [=, &f0](float a) { return f0 += f1 + std::abs(a); } (-3.5);
+    std::cout << '\n' << f0 << '\n';
+//f0 通过引用被捕获，而其它变量，比如 f1 则是通过值被捕获。
+//    []        // 不捕获任何外部变量å
+//    [=]      // 以值的形式捕获所有外部变量
+//    [&]      // 以引用形式捕获所有外部变量
+//    [x, &y] // x 以传值形式捕获，y 以引用形式捕获
+//    [=, &z]// z 以引用形式捕获，其余变量以传值形式捕获
+//    [&, x]  // x 以值的形式捕获，其余变量以引用形式捕获
+    
     return 0;
 }
